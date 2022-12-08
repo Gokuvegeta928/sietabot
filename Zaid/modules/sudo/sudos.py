279acc067e50f21e5a0c0ef537cd61cee72746b6
Gokuvegeta928 <120127207+Gokuvegeta928@users.noreply.github.com>
2022-12-08 23:32:01 +0530

    Update sudos.py

--- Zaid/modules/sudo/sudos.py
+++ Zaid/modules/sudo/sudos.py
@@ -1,100 +1,36 @@
-from pyrogram import Client, errors, filters
-from pyrogram.types import ChatPermissions, Message
-DEVS = int(1669178360)
-from Zaid.helper.PyroHelpers import get_ub_chats
-from Zaid.modules.basic.profile import extract_user, extract_user_and_reason
-from Zaid import SUDO_USER
-from config import OWNER_ID
-from Zaid.modules.help import add_command_help
+from pyrogram import Client
+from pyrogram.types import Message
+hl = "."
 
-ok = []
-
-
-@Client.on_message(filters.command("sudolist", ".") & filters.me)
-async def gbanlist(client: Client, message: Message):
-    users = (SUDO_USER)
-    ex = await message.edit_text("`Processing...`")
-    if not users:
-        return await ex.edit("No Users have been set yet")
-    gban_list = "**Sudo Users:**\n"
-    count = 0
-    for i in users:
-        count += 1
-        gban_list += f"**{count} -** `{i}`\n"
-    return await ex.edit(gban_list)
-
-
-@Client.on_message(filters.command("addsudo", ".") & filters.user(OWNER_ID))
-async def gmute_user(client: Client, message: Message):
-    args = await extract_user(message)
-    reply = message.reply_to_message
-    ex = await message.reply_text("`Processing...`")
-    if args:
-        try:
-            user = await client.get_users(args)
-        except Exception:
-            await ex.edit(f"`Please specify a valid user!`")
-            return
-    elif reply:
-        user_id = reply.from_user.id
-        user = await client.get_users(user_id)
-    else:
-        await ex.edit(f"`Please specify a valid user!`")
+async def add_or_del_sudo(_, m):
+    me = await _.get_me()
+    if m.from_user.id != me.id:
         return
-    if user.id == client.me.id:
-        return await ex.edit("**Okay Sure.. 🐽**")
-
     try:
-        if user.id in SUDO_USER:
-            return await ex.edit("`User already in sudo`")
-        SUDO_USER.append(user.id)
-        await ex.edit(f"[{user.first_name}](tg://user?id={user.id}) Added To Sudo Users!")
-    
-    except Exception as e:
-        await ex.edit(f"**ERROR:** `{e}`")
+        id = await get_id(m)
+    except:
+        return await eor(m, f"<i>{hl}addsudo or {hl}rmvsudo [Reply | Username | Id]</id>")
+    sudo = await is_sudo(id)
+    if m.text.split()[0][1].lower() == "r":
+        if not sudo:
+            return await eor(m, f"<i>This user isn't sudo..!</i>")
+        await del_sudo(id)
+        return await eor(m, f"<i>Sudo removed for the user {id} .</i>")
+    if sudo:
+        return await eor(m, f"<i>{id} is already a sudo user..!</i>")
+    await add_sudo(id)
+    return await eor(m, f"<i>{id} is added to sudo...!</i>")
+
+async def sudo_users(_, m):
+    me = await _.get_me()
+    sudo = await is_sudo(m.from_user.id)
+    if m.from_user.id != me.id and not sudo:
         return
-
-
-@Client.on_message(filters.command("rmsudo", ".") & filters.user(OWNER_ID))
-async def gmute_user(client: Client, message: Message):
-    args = await extract_user(message)
-    reply = message.reply_to_message
-    ex = await message.reply_text("`Processing...`")
-    if args:
-        try:
-            user = await client.get_users(args)
-        except Exception:
-            await ex.edit(f"`Please specify a valid user!`")
-            return
-    elif reply:
-        user_id = reply.from_user.id
-        user = await client.get_users(user_id)
-    else:
-        await ex.edit(f"`Please specify a valid user!`")
-        return
-    if user.id == client.me.id:
-        return await ex.edit("**Okay Sure.. 🐽**")
-
-    try:
-        if user.id in SUDO_USER:
-            return await ex.edit("`User is not a part of sudo`")
-        SUDO_USER.remove(user.id)
-        await ex.edit(f"[{user.first_name}](tg://user?id={user.id}) Removed To Sudo Users!")
-    
-    except Exception as e:
-        await ex.edit(f"**ERROR:** `{e}`")
-        return
-
-
-
-add_command_help(
-    "sudos",
-    [
-        [
-            "addsudo <reply/username/userid>",
-            "Add any user as Sudo (Use This At your own risk maybe sudo users can control ur account).",
-        ],
-        ["rmsudo <reply/username/userid>", "Remove Sudo access."],
-        ["sudolist", "Displays the Sudo List."],
-    ],
-)
+    SUDOS = await get_sudos()
+    if not SUDOS:
+        return await eor(m, f"<i>No sudo users..!</i>")
+    msg = ""
+    for SUDO in SUDOS:
+        SUDO = int(SUDO)
+        msg += f"\n<code>{SUDO}</code>"
+    return await eor(m, f"<i>Sudo :-</i>\n{msg}\n\n<i>Count :- {len(SUDOS)}</i>")
