e8860cd3b4649b231fa8f66db597790add36e483
Gokuvegeta928 <120127207+Gokuvegeta928@users.noreply.github.com>
2022-12-08 23:37:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "8460373")) #optional
+API_HASH = getenv("API_HASH", "83d8e423197251216303abfcbed9e820") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1233641093").split()))
+OWNER_ID = int(getenv("OWNER_ID", "1233641093"))
+MONGO_URL = getenv("MONGO_URL", ", "mongodb+srv://Lightyagami10:light@cluster0.zo6bf4g.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5942608707:AAHMVoTnnauHTdFxB5unFlGFcNNWjIT9BOU")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/0fc684fcc026189c1922a.mp4")
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
@@ -15,7 +15,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkAjProgG0XE6UEZ075azMaIAO2pmvQoLXFI-h525MuN8zlVNMDB59gOM0JgBWhT5go4Lkq1AJL2VAYW3iuru13LVzUrywm4LqQSVfMVrDQHd4ks_0M4nIyqVSH9G1n7iaRdUL5pfyCx9yUDQIlE5bagEBuUzTd0eWCqmA4qC4seD3TxwNg_bCOV92rOufPERYaeEqm5xmeRVNNGa_NXi29nm2fUQWpqfkBk_mAftFL7LAPwrW8612-2BJOXp9VhvNMlCAoKqxQg94PPebjHgBqmFC8bn3ejWk3dqIJ8KdNsN1pzQ81jBYHjQK2B21TcyEK5zCgMAxNFr89GpTpob6CRAAAAABJh96FAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
